f2a70c2544635095d00c0ae5de9bd5c0fb7271a1
Edouard Klein <edouard.klein@supelec.fr>
2012-12-06 18:58:33 +0100

    Less verbose, prints error message when RTEcompile fails

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ install:
 	cp startRTE.sh $(DEST)/
 	cp stopRTE.sh $(DEST)/
 	cp RTEAgent.py $(DEST)/
-	cat RTE.el >> ~/.emacs #toupouri
+#	cat RTE.el >> ~/.emacs #toupouri
 	mkdir -p  ~/.vim/plugin
 	cp RTE.vim ~/.vim/plugin
--- startRTE.sh
+++ startRTE.sh
@@ -1,3 +1,3 @@
-#RTEFS.py /tmp/mnt && (nohup emacs Makefile --funcall launch-RTE  &); sleep 1
-RTEFS.py /tmp/mnt && (nohup gvim Makefile -c "call UpdateFile()" &); sleep 1
+RTEFS.py /tmp/mnt 2>/dev/null & (nohup emacs Makefile --funcall launch-RTE  &); sleep 1
+#RTEFS.py /tmp/mnt && (nohup gvim Makefile -c "call UpdateFile()" &); sleep 1
 exit
